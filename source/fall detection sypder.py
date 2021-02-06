5715ebe882438fc67ed47a363e041695060bbed9
Thadka S. Keerthika <tonnynoddy987@gmail.com>
2021-02-07 01:33:18 +0530

    Second Commit

--- source/fall detection sypder.py
+++ source/fall detection sypder.py
@@ -5,12 +5,13 @@ Created on Sun Jan 31 03:35:03 2021
 @author: Admin
 """
 
+from twilio.rest import Client
 import cv2
 import time
 
 fitToEllipse = False
 cap = cv2.VideoCapture(r'E:\SB\Fall Detection\Data Collection\demo1.mp4')
-time.sleep(2)   
+time.sleep(2)
 
 fgbg = cv2.createBackgroundSubtractorMOG2()
 j = 0
@@ -36,19 +37,33 @@ while(1):
                 areas.append(ar)
             
             max_area = max(areas, default = 0)
+
             max_area_index = areas.index(max_area)
+
             cnt = contours[max_area_index]
-            M = cv2.moments(cnt)          
+
+            M = cv2.moments(cnt)
+            
             x, y, w, h = cv2.boundingRect(cnt)
-            cv2.drawContours(fgmask, [cnt], 0, (255,255,255), 5, maxLevel = 0)           
+
+            cv2.drawContours(fgmask, [cnt], 0, (255,255,255), 3, maxLevel = 0)
+            
             if h < w:
                 j += 1
                 
-            if j > 20:
+            if j > 10:
                 print("FALL")
-                cv2.putText(fgmask, 'FALL', (x+5, y-5), cv2.FONT_HERSHEY_TRIPLEX, 2, (79,244,255), 2)
-                cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+                cv2.putText(fgmask, 'FALL', (x, y), cv2.FONT_HERSHEY_TRIPLEX, 1, (255,255,255), 2)
+                cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),3)
                 
+                t_s="ACc51294ea27ad1464b3e7b6000742c46c"
+                t_a = "92f88891df24eb7dacdbc983d6c1e07e"
+                client = Client(t_s,t_a)
+                TO = "+917801028373"
+                FROM="+12067923295"
+                client.messages.create(to=TO,from_=FROM,body="Your Kid has FALLEN DOWN......(OR).....Your Parent has FALLEN DOWN")
+                time.sleep(2000)
+
             if h > w:
                 j = 0 
                 cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
@@ -56,7 +71,7 @@ while(1):
 
             cv2.imshow('video', frame)
         
-            if cv2.waitKey(1) & 0xFF == ord('q'):
+            if cv2.waitKey(33) == 27:
              break
     except Exception as e:
         break
